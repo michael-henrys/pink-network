b7eb2d9fbabdb3a15228f4cfd0132d21c135de0e
Michael Henrys <89361587+michael-henrys@users.noreply.github.com>
2022-01-26 15:08:15 +1300

    Fix merge conflict

--- client/components/LandingPage/LandingPage.jsx
+++ client/components/LandingPage/LandingPage.jsx
@@ -1,14 +1,5 @@
-<<<<<<< Updated upstream
-import React from 'react'
-
-export default function LandingPage () {
-  return <div></div>
-}
-||||||| constructed merge base
-=======
 import React from 'react'
 
 export default function LandingPage () {
   return <div className={LandingPage}></div>
 }
->>>>>>> Stashed changes
