b942524511b782242a56947e021b44ac313c1e6f
Michael Henrys <89361587+michael-henrys@users.noreply.github.com>
2022-01-26 21:27:45 +1300

    Added empty components and router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Archive from './Archive/Archive'
+import ArchiveItem from './ArchiveItem/ArchiveItem'
+
+import LandingPage from './LandingPage/LandingPage'
+import Home from './Home/Home'
+import Payment from './Payment/Payment'
+import Confirmed from './Confirmed/Confirmed'
 
 function App () {
   return (
     <>
-      <header className="header">
-        <h1>My Collection</h1>
-      </header>
-      <section className="main">
-        {/* add your code here */}
-      </section>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="safety-code" element={<safetyCode />} />
+        <Route path="home" element={<Home />} />
+        <Route path="payment" element={Payment} />
+        <Route path="confirmed" element={Confirmed} />
+        <Route path="archive" element={<Archive/>} />
+        <Route path="archive/:nightId" element={<ArchiveItem />} />
+      </Routes>
     </>
   )
 }
---
+++ client/components/Confirmed/Confirmed.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Confirmed () {
+  return <div></div>
+}
