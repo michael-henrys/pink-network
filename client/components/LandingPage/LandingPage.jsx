4cbd69ef0ccd647f681e8a664e78bc38ae5c9307
Michael Henrys <89361587+michael-henrys@users.noreply.github.com>
2022-02-10 15:29:33 +1300

    landing page styling and layout completed

--- client/components/LandingPage/LandingPage.jsx
+++ client/components/LandingPage/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Button from '../utils/Button'
+import LOGO from '../../images/LOGO.svg'
+import GRAPHIC from '../../images/GOBLIN.svg'
 
 export default function LandingPage () {
   return (
     <div className='landing-page'>
-      <h1>TitleLogo</h1>
-      <h2>Graphic</h2>
+      <img className='title-logo' alt='Decorated title with Pink Network text' src={LOGO}/>
+      <img className='graphic' alt='Graphic' src={GRAPHIC}/>
       <Button destination='/safety-code' label='ENTER' />
     </div>
   )
--- client/components/utils/Button.jsx
+++ client/components/utils/Button.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Button ({ destination, label }) {
   return (
     <Link to={destination}>
-      <button className='button'>{label}</button>
+      <div className='button'>{label}</div>
     </Link>
   )
 }
