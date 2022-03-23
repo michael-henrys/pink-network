ca7035f234358af488a38aebcab7ffef260ebe73
Michael Henrys <89361587+michael-henrys@users.noreply.github.com>
2022-03-23 21:14:34 +1300

    Added star and ticker to payment page

--- client/components/Payment/Payment.jsx
+++ client/components/Payment/Payment.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Modal from '../utils/Modal'
 import Ticker from '../utils/Ticker'
+import Line from '../utils/Line'
 
 export default function Payment () {
   return (
     <Modal>
       <Ticker />
+      <div className='payment-container'>
+        <br></br>
+        <Line position="top" color="black"/>
+      </div>
     </Modal>
   )
 }
