4c37a2acc8989ec1de0fe41a161aa5ce4356f402
Michael Henrys <89361587+michael-henrys@users.noreply.github.com>
2022-03-29 17:05:42 +1300

    begin adding stripe to frontend

--- client/components/Payment/Payment.jsx
+++ client/components/Payment/Payment.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Modal from '../utils/Modal'
 import Ticker from '../utils/Ticker'
 import Line from '../utils/Line'
 import PriceSelector from './PriceSelector'
+import { useSelector } from 'react-redux'
+import loadStripe from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import PaymentForm from './PaymentForm'
 
 export default function Payment () {
+  const [clientSecret, setClientSecret] = useState('')
+
+  const price = useSelector(state => state.price)
+
+  const stripePromise = loadStripe('pk_test_51KiV8jH6CDpmUuG8WQGpjokLPejgX5ovFiVTeQbnimydSpjEPIsBBxuWXBJnsPwppUiUpcfWqH47iBtIQmAAVENS00IvIaCkdx')
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch('/create-payment-intent', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ price })
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret))
+      .catch((err) => console.error(err))
+  }, [])
+
+  const appearance = {
+    theme: 'stripe'
+  }
+  const options = {
+    clientSecret,
+    appearance
+  }
+
   return (
     <Modal>
       <Ticker />
@@ -12,6 +42,11 @@ export default function Payment () {
         <br></br>
         <Line position="top" color="black"/>
         <PriceSelector />
+        {clientSecret && (
+          <Elements options={options} stripe={stripePromise}>
+            <PaymentForm/>
+          </Elements>
+        )}
       </div>
     </Modal>
   )
